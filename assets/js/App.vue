0675251358e9074fc23c4bb6cc434dafc1184227
Thomas <thomas.trompette@sfr.fr>
2018-06-06 22:08:17 +0200

    Pop tasks

--- assets/js/App.vue
+++ assets/js/App.vue
@@ -8,6 +8,9 @@
                 <md-list>
                     <md-list-item v-for="task in tasks">
                         <span class="md-list-item-text">{{ task.name }}</span>
+                        <md-button class="md-icon-button" v-on:click="deleteTask">
+                            <md-icon>delete</md-icon>
+                        </md-button>
                     </md-list-item>
                 </md-list>
             </md-app-content>
@@ -17,6 +20,18 @@
 
 <script>
  export default {
-     name: 'app'
+     name: 'app',
+     data: () => ({
+        tasks: []
+     }),
+     mounted: function() {
+         this.tasks = window.__TASKS
+     },
+     methods: {
+         deleteTask: function () {
+             this.tasks.pop()
+             console.log('hello')
+         }
+     }
  }
 </script>
--- assets/js/main.js
+++ assets/js/main.js
@@ -7,16 +7,6 @@ import 'vue-material/dist/theme/default.css' // This line here
 
 Vue.use(VueMaterial)
 
-Vue.mixin({
-    data: function () {
-        return {
-            get tasks() {
-                return window.__TASKS
-            }
-        }
-    }
-})
-
 new Vue({
     el: '#app',
     template: '<App/>',
